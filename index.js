5a35b2b2bef45f42c235e0cbc8c15a913196c1cf
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-06-07 20:08:26 -0300

    version estable

--- index.js
+++ index.js
@@ -19,7 +19,27 @@ const punto = document.getElementById("punto");
 const igual = document.getElementById("igual");
 
 let operador = "";
+let memoria = 0;
 let resultado = 0;
+
+function resolver() {
+    if(operador === "+"){
+        resultado = memoria + parseFloat(display.value);
+    }
+    else if(operador === "-"){
+        resultado = memoria - parseFloat(display.value);
+    }
+    else if(operador === "x"){
+        resultado = memoria * parseFloat(display.value);
+    }
+    else if(operador === "÷"){
+        resultado = memoria / parseFloat(display.value);
+    }
+    
+    display.value = `${resultado} = ` ;
+    displaySecundario.value = parseFloat(display.value);
+    memoria = 0;
+}
 
 function verificarDisplay() {
     if(display.value === ""){
@@ -27,96 +47,139 @@ function verificarDisplay() {
     }
 }
 
+function reiniciarDisplay() {
+    if(display.value === resultado + " = "){
+        display.value = "";
+    }
+}
+
 function reiniciarDisplaySecundario(){
-    if(resultado === 0){
-        displaySecundario.value = "";
-    }
+    displaySecundario.value = "";
+   
 }
 
-function actualizarDisplaySecundario(){
-    displaySecundario.value += parseFloat(display.value);
-}
 
-function resolver() {
-    if(display.value === ""){
-        resultado += 0;
+sumar.addEventListener("click", () => {
+    if(display.value !== ""){
+        operador = "+";
+        memoria += parseFloat(display.value);
+        displaySecundario.value += operador;
+        display.value = "";   
     }
-    else if(operador === "+"){
-        resultado += parseFloat(display.value);
+});
+
+
+
+restar.addEventListener("click", () => {
+    if((display.value !== "") && (displaySecundario.value.length < 2)){
+        operador = "-";
+        memoria = parseFloat(display.value);
+        displaySecundario.value += operador;
+        display.value = "";
     }
-    else if(operador === "-"){
-        resultado -= parseFloat(display.value);
+});
+
+
+
+multiplicar.addEventListener("click", () => {
+    if((display.value !== "") && (displaySecundario.value.length < 2)){
+        operador = "x";
+        memoria = parseFloat(display.value);
+        displaySecundario.value += operador;
+        display.value = "";
     }
-    else if(operador === "x"){
-        resultado = resultado * parseFloat(display.value);
+});
+
+
+
+dividir.addEventListener("click", () => {
+    if((display.value !== "") && (displaySecundario.value.length < 2)){
+        operador = "÷";
+        memoria = parseFloat(display.value);
+        displaySecundario.value += operador;
+        display.value = "";
     }
-    else if(operador === "÷"){
-        resultado = resultado / parseFloat(display.value);
-    }
-    
-    display.value = resultado + " = ";
-    resultado = 0;
-    
-}
+});
+
 
 
 uno.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += uno.value;
-})
+    displaySecundario.value += uno.value;
+});
 
 
 
 dos.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += dos.value;
-})
+    displaySecundario.value += dos.value;
+});
 
 
 
 tres.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += tres.value;
-})
+    displaySecundario.value += tres.value;
+});
 
 
 
 cuatro.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += cuatro.value;
-})
+    displaySecundario.value += cuatro.value;
+});
 
 
 
 cinco.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += cinco.value;
-})
+    displaySecundario.value += cinco.value;
+});
 
 
 
 seis.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += seis.value;
-})
+    displaySecundario.value += seis.value;
+});
 
 
 
 siete.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += siete.value;
-})
+    displaySecundario.value += siete.value;
+});
 
 
 
 ocho.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += ocho.value;
-})
+    displaySecundario.value += ocho.value;
+});
 
 
 
 nueve.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += nueve.value;
-})
+    displaySecundario.value += nueve.value;
+});
 
 
 
 cero.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += cero.value;
-})
+    displaySecundario.value += cero.value;
+});
 
 
 
@@ -124,68 +187,27 @@ borrar.addEventListener("click", () => {
     display.value = "";
     displaySecundario.value = "";
     resultado = 0;
-})
+    memoria = 0;
+});
 
 
 
 punto.addEventListener("click", () => {
+    reiniciarDisplay();
     display.value += punto.value;
-})
-
-
-
-sumar.addEventListener("click", () => {
-    reiniciarDisplaySecundario();
-    if(display.value !== ""){
-        operador = "+";
-        resultado += parseFloat(display.value);
-        displaySecundario.value += parseFloat(display.value) + operador;
-        display.value = "";   
-    }
-})
-
-
-
-restar.addEventListener("click", () => {
-    reiniciarDisplaySecundario();
-    if(display.value !== ""){
-        operador = "-";
-        resultado += parseFloat(display.value);
-        displaySecundario.value += parseFloat(display.value) + operador;
-        display.value = "";
-    }
-})
-
-
-
-multiplicar.addEventListener("click", () => {
-    reiniciarDisplaySecundario();
-    if(display.value !== ""){
-        operador = "x";
-        resultado = parseFloat(display.value);
-        displaySecundario.value += parseFloat(display.value) + operador;
-        display.value = "";
-    }
-})
-
-
-
-dividir.addEventListener("click", () => {
-    reiniciarDisplaySecundario();
-    if(display.value !== ""){
-        operador = "÷";
-        resultado = parseFloat(display.value);
-        displaySecundario.value += parseFloat(display.value) + operador;
-        display.value = "";
-    }
-})
-
-
+});
 
 
 
 igual.addEventListener("click", () => {
+    if(display.value === ""){
+        memoria += 0;
+    }
     verificarDisplay();
-    actualizarDisplaySecundario();
+    reiniciarDisplaySecundario();
     resolver();
-})
+});
+
+function hola(){
+    console.log("hola");
+}
